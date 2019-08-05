c6f61bf6a91065cee12a35d49cff67120bc64c10
kennedyMukhari <45757080+kennedyMukhari@users.noreply.github.com>
2019-08-05 08:47:07 +0200

    Login Interface

---
+++ src/app/view-rooms/view-rooms.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-rooms',
+  templateUrl: './view-rooms.page.html',
+  styleUrls: ['./view-rooms.page.scss'],
+})
+export class ViewRoomsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
